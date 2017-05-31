f833a919afd415c8f44e616cd9040cf3a0d9576f
Juan Carlos Rudin <juan.rudin@proximitycr.com>
2017-05-31 13:14:57 -0600

    Pequeno cambio.

--- Conservatorio.DATOS/PagoMensualidad.cs
+++ Conservatorio.DATOS/PagoMensualidad.cs
@@ -8,7 +8,7 @@ namespace Conservatorio.DATOS
         public virtual float Monto { get; set; }
         public virtual float Multa { get; set; }
         public virtual DateTime FechaPago { get; set; }
-        public virtual String Mes { get; set; }
+        public virtual string Mes { get; set; }
         public virtual Estudiante Estudiante { get; set; }
         public virtual string Comentario { get; set; }
         public virtual string TipoPago { get; set; }
