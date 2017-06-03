8956b9adba8d0e2c83ce559e2b7e044abe2e497e
Gabriela Sanchez <gabskatze@gmail.com>
2017-06-02 22:17:50 -0600

    Notificacion error al eliminar curso

--- Conservatorio.BL/Clases/CursoBL.cs
+++ Conservatorio.BL/Clases/CursoBL.cs
@@ -3,6 +3,7 @@ using Conservatorio.BL.Interfaces;
 using Conservatorio.DATOS;
 using Conservatorio.DS.Clases;
 using Conservatorio.DS.Interfaces;
+using System.Linq;
 
 namespace Conservatorio.BL.Clases
 {
@@ -26,6 +27,12 @@ namespace Conservatorio.BL.Clases
 
         public void EliminarCurso(Curso curso)
         {
+            List<string> mensajes;
+            if (!ValidarEliminarCurso(curso, out mensajes))
+            {
+                throw new ValidacionException(mensajes);
+            }
+
             _cursoDs.EliminarCurso(curso);
         }
 
@@ -37,6 +44,22 @@ namespace Conservatorio.BL.Clases
         public List<Curso> ObtenerCursosPorInstrumento(int idInstrumento)
         {
             return _cursoDs.ObtenerCursos(x => x.Instrumento.IdInstrumento == idInstrumento);
+        }
+
+        private bool ValidarEliminarCurso(Curso curso, out List<string> msjs)
+        {
+            var result = true;
+            msjs = new List<string>();
+
+            var cursos = ObtenerCursos();
+
+            if (cursos.Any(x => x.CursoRequisito != null && x.CursoRequisito.IdCurso == curso.IdCurso))
+            {
+                result = false;
+                msjs.Add("El curso que desea eliminar es requisito de otro.");
+            }
+
+            return result;
         }
     }
 }
